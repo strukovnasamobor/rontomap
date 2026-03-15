239cf06e4ca1b00cc2b474fd0033ff772fa23ec8
ZDamijan <zlatko.damijanic@gmail.com>
2026-03-15 10:16:34 +0100

    Add header logo with link to Page Not Found component

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,6 +3,9 @@ import "./PageNotFound.css";
 export default function PageNotFound() {
   return (
     <div className="page-not-found full-screen center">
+      <a href="/">
+        <img class="header-logo" src="/assets/header.png" alt="Rontomap" />
+      </a>
       <p>Page Not Found</p>
     </div>
   );
